2560d4cb2dc10efa3c6acd65021d54b89d63b596
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-10 18:45:16 -0600

    first commit

---
+++ candlecommapi/models/profile.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=140)
+    

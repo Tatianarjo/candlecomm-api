783c3470440ad4eae343fa7714ce9df3c8e9f546
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-15 20:45:59 -0500

    can GET candles

--- candlecomm/urls.py
+++ candlecomm/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from candlecommapi.views import register_user, login_user
 from rest_framework import routers
 from candlecommapi.views import ScentView
+from candlecommapi.views import CandleView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'scents', ScentView, 'scent')
+router.register(r'candles', CandleView, 'candle')
 
 
 urlpatterns = [

6e97f056aed2c1575b4b050d179b965e07e540d7
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-21 19:20:02 -0500

    Revert "working on multiple selections for candles scents"
    This reverts commit db871b098fbb76990c8f7866d4397acd5c0dc9f6.

--- candlecommapi/models/candle.py
+++ candlecommapi/models/candle.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Candle(models.Model):
     candle_name = models.CharField(max_length=100)
-    scent = models.ManyToManyField("candlecommapi.scent")
+    scent = models.ForeignKey("candlecommapi.scent", on_delete=models.CASCADE, default=1)
     profile = models.ForeignKey("candlecommapi.profile", on_delete=models.DO_NOTHING)
     jar_color = models.ForeignKey("candlecommapi.jarcolor", on_delete=models.SET_NULL, null=True)
     upload = models.ForeignKey("candlecommapi.upload", on_delete=models.SET_NULL, null=True)
